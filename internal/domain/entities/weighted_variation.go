24a27f969383395a66317a4afc3ff6bc23ad98a4
kurochkinivan <vanya.kurochkin.23@mail.ru>
2025-12-14 19:55:40 +0700

    feat: add weighted variation entity

---
+++ internal/domain/entities/weighted_variation.go
@@ -0,0 +1,23 @@
+package entities
+
+import (
+	"fmt"
+
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/domain/variation"
+)
+
+type WeightedVariation struct {
+	variation.NamedFunction
+	Weight float64
+}
+
+func NewWeightedVariation(namedFunction variation.NamedFunction, weight float64) WeightedVariation {
+	return WeightedVariation{
+		NamedFunction: namedFunction,
+		Weight:        weight,
+	}
+}
+
+func (w WeightedVariation) String() string {
+	return fmt.Sprintf("%s(%.2f)", w.Name, w.Weight)
+}
