dc5453a3cd240f764603cc1613f5b1cec085cddd
kurochkinivan <vanya.kurochkin.23@mail.ru>
2025-12-14 19:59:52 +0700

    feat: add utils for png image saving

---
+++ internal/infrastructure/image_utils/png_saver.go
@@ -0,0 +1,29 @@
+package image_utils
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+)
+
+type PNGSaver struct{}
+
+func NewImageSaver() *PNGSaver {
+	return &PNGSaver{}
+}
+
+// SaveImage saves the given image to the specified path as a PNG file.
+func (i *PNGSaver) SaveImage(img image.Image, path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("failed to create file %q: %w", path, err)
+	}
+	defer f.Close()
+
+	if err := png.Encode(f, img); err != nil {
+		return fmt.Errorf("failed to encode png: %w", err)
+	}
+
+	return nil
+}
---
+++ internal/infrastructure/image_utils/png_saver_test.go
@@ -0,0 +1,25 @@
+package image_utils_test
+
+import (
+	"image"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/hw4-fractal-flame/internal/infrastructure/image_utils"
+)
+
+func TestSaveImageHappyPath(t *testing.T) {
+	saver := image_utils.NewImageSaver()
+
+	img := image.NewRGBA(image.Rect(0, 0, 1, 1))
+	path := filepath.Join(t.TempDir(), "out.png")
+
+	if err := saver.SaveImage(img, path); err != nil {
+		t.Fatalf("failed: %v", err)
+	}
+
+	if _, err := os.Stat(path); err != nil {
+		t.Fatalf("file not created: %v", err)
+	}
+}
